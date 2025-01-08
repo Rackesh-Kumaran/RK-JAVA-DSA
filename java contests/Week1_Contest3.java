f33efb1cdf5ab9eb2927a69d951576b5a17c8656
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-08 15:09:45 +0530

    Add files via upload
    Write a program to print N numbers starting from 2. Take N as input and print from 2 to N;
    
    Input Format
    
    6
    
    Constraints
    
    3<=N<=10^3
    
    Output Format
    
    2 3 4 5 6

---
+++ java contests/Week1_Contest3.java
@@ -0,0 +1,18 @@
+package practice_Java_DSA;
+
+import java.util.Scanner;
+
+public class Week1_Contest3 {
+	public static void main(String[] args) {
+		
+		System.out.println("enter the inputs:");
+		
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i=2;i<=N;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
