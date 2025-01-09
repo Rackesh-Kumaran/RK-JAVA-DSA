6634b20c7b66caf75d8a01029198cd723299ee6f
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-09 14:46:04 +0530

    Add files via upload
    Example 1:
    
    Input: 5
    
    Output:
    
        *
       ***  
      *****
     *******
    *********

---
+++ java patterns/Week2_Pattern10.java
@@ -0,0 +1,24 @@
+package practice_Java_DSA;
+
+import java.util.Scanner;
+
+public class Week2_Pattern10 {
+	public static void main(String[] args) {
+		System.out.println("Enter the inputs: ");
+		
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=N-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<= 2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
