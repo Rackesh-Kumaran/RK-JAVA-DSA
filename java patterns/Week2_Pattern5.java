359164511d668573a2cc5e07fb67dddb6e3346e8
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-09 13:43:01 +0530

    Add files via upload
    pattern 5:
    
    Input:
    
    n = 4
    
    Expected output:
    
    1 2 3 4
    
    1 2 3 
    
    1 2 
    
    1

---
+++ java patterns/Week2_Pattern5.java
@@ -0,0 +1,20 @@
+package practice_Java_DSA;
+
+import java.util.Scanner;
+
+public class Week2_Pattern5 {
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the inputs: ");
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=N-i+1;j++) {
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
