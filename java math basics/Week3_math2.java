7efdc58b1c5510f34d22caafe838d8e930c9a450
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-24 11:51:52 +0530

    Add files via upload
    last digit of number 
    
    input : 1234
    output : 4

---
+++ java math basics/Week3_math2.java
@@ -0,0 +1,22 @@
+package practice_Java_DSA;
+
+import java.util.Scanner;
+
+public class Week3_math2 {
+	public static void main (String[] args) {
+		
+		System.out.println("enter the inputs: ");
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		while(n>0) {
+			int ld = n%10;
+			System.out.println(ld);
+			
+			n = n/10;
+		}
+		
+	}
+
+}
