0f5ef2fcaf263d8837ede8719fce2036a08f9148
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-31 09:27:26 +0530

    Add files via upload
    Sample Input N = 3
    
    Sample Output :
    
       *
      ***
     *****
     *****
      ***
       *

---
+++ java contests/Week2_Contest2.java
@@ -0,0 +1,48 @@
+package practice_Java_DSA;
+
+
+import java.util.Scanner;
+
+public class Week2_Contest2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("enter the inputs : ");
+		
+		 Scanner scan = new Scanner(System.in);
+	     int N = scan.nextInt();
+	     
+
+	        for (int i = 1; i <= N; i++) {
+	           
+	            for (int j = 1; j <= N - i; j++) {
+	                System.out.print(" ");
+	            }
+	          
+	            for (int j = 1; j <= 2 * i - 1; j++) {
+	                System.out.print("*");
+	            }
+	            
+	            System.out.println();
+	        }
+	        
+	        
+	        
+	        
+	        for (int i = N; i >= 1; i--) {
+	            
+	            for (int j = 1; j <= N - i; j++) {
+	                System.out.print(" ");
+	            }
+	          
+	            for (int j = 1; j <= 2 * i - 1; j++) {
+	                System.out.print("*");
+	            }
+	            
+	            System.out.println("");
+	        }
+	        
+	        
+	}
+
+}
