6f35a6489c21ff1c517208923b5cc40090b812b9
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-02-18 15:42:04 +0530

    Add files via upload
    Add and count the values in an array:
    
    input = 1,2,3,4,5
    output = 6,7,8,9,10
    
    explanation :
    iteration 1: i = 0 , 0< 5 
    a[0] = 1 
    a[0]+5 =(1+5);
    o/p = 6 
    
    like wise all the iteration will be done

---
+++ java arrays/Week4_array03.java
@@ -0,0 +1,25 @@
+package practice_Java_DSA;
+
+import java.util.Scanner;
+
+public class Week4_array03 {
+	public static void main(String[] args) {
+		System.out.println("enter the inputs:");
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		 int []arr = new int[n];
+		 
+		 for(int i=0;i<n;i++) {
+			 arr[i] = sc.nextInt(); 
+		 }
+		 
+		 for(int i=0;i<n;i++) {
+				arr[i] = arr[i] + 5;
+			    System.out.println(arr[i]);
+		 }
+		
+	}
+
+}
